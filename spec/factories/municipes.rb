25882dcdec80d2f886106cdeacda86a50824bee0
Hemershon Silva <hemershon@gmail.com>
2024-03-05 19:52:07 -0300

    feat create model municipe and address

---
+++ spec/factories/municipes.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :municipe do
+    nome_completo { "MyString" }
+    cpf_cns { "MyString" }
+    email { "MyString" }
+    data_nascimento { "2024-03-05" }
+    telefone { "MyString" }
+    foto { "MyString" }
+    status { false }
+  end
+end
