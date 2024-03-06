91eddf8dc520aa6ca0271ba59bd8cd6c60d1bee0
Hemershon Silva <hemershon@gmail.com>
2024-03-06 09:54:15 -0300

    feat add text for model municipe

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: enderecos
+#
+#  id          :bigint           not null, primary key
+#  bairro      :string
+#  cep         :string
+#  cidade      :string
+#  codigo_ibge :string
+#  complemento :string
+#  logradouro  :string
+#  uf          :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  municipe_id :bigint           not null
+#
+# Indexes
+#
+#  index_enderecos_on_municipe_id  (municipe_id)
+#
+# Foreign Keys
+#
+#  fk_rails_8abd9faee9  (municipe_id => municipes.id)
+#
 class Endereco < ApplicationRecord
   belongs_to :municipe
 end
